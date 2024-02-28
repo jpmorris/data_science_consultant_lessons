278069ddc6ce1f48f02dbaae5b6727f4dd08fda9
James Morris <me@james-morris.com>
2024-02-28 10:31:15 -0500

    staring genai

--- 003-genai/003-02-genai-aws.py
+++ 003-genai/003-02-genai-aws.py
@@ -1,31 +1,31 @@
 # %% [markdown]
 # ## GenAI Products in AWS
 # - Sagemaker Bedrock - Managed service, higher-level, less-control
-# - Sagemaker Jumpstart - Lower-level, more control, more flexibility 
+# - Sagemaker Jumpstart - Lower-level, more control, more flexibility
 
 # %% [markdown]
 # ## Big question that we always should ask:
 # ### Should we even do this?
-# There is a tendency to take the most avant-garde technology and apply it to 
+# There is a tendency to take the most avant-garde technology and apply it to
 # an organization. Our data may not be robust enough, the technology not advanced
-# enough, or the organization not ready enough. 
+# enough, or the organization not ready enough.
 #
 #
-# Fine-tuned (therefor not a general model) LLMs cant currently (if ever) outperform a 
-# other 
+# Fine-tuned (therefor not a general model) LLMs cant currently (if ever) outperform a
+# other
 # %% [markdown]
 # ## Some use Cases for in-house GenAI for a federal oversight agency
 # Naive, but desirable prompts:
-# - 'Where is fraud happening in my agency?' - Fraud signal is too weak to 
+# - 'Where is fraud happening in my agency?' - Fraud signal is too weak to
 # - 'What are the common connections between two auditees?' -
-# - 'How many findings of an audit were there for type X' - 
-# detect fraud so easily, 
-# By ingesting audit data: 
+# - 'How many findings of an audit were there for type X' -
+# detect fraud so easily,
+# By ingesting audit data:
 # - Generating summaries
 # - Generating reports from audit data
 # - Generating responses to common inquiries
 #
-#  
+#
 
 # %%
 # # Steps to fine-tune a LLM with custom data for an organization
