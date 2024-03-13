9cb96c51d735f324889347771977ee6d9c7f98cc
James Morris <me@james-morris.com>
2024-03-13 11:27:38 -0400

    testing lecture

--- lectures/002-best_programming_practices/002-02-avoid-mocking.py
+++ lectures/002-best_programming_practices/002-02-avoid-mocking.py
@@ -1,14 +1,38 @@
 # %% [markdown]
-# # Which is better to unit testing?
-# ```
-# def function(file_name):
-#     stuff = open(file_name)
-#     file_contents = do_stuff(stuff)
-# ```
+# # Given the following code snippets:
+
+
+# %%
+def work_with_file(file_name):
+    with open(file_name) as fp:
+        file_contents = fp.read()
+        do_stuff(risky_stuff)
+
+
+# %% [markdown]
 # or
-# ```
-# def function(file_contents):
-#     do_stuff(file_contents)
+
+# %%
+with open(file_name) as fp:
+    file_contents = fp.read()
+
+
+def function(file_contents):
+    do_stuff(file_contents)
+
+
+# %% [markdown]
+# # Questions:
+# 1. Which seems better?
+# <details>
+#  <summary>Answer</summary>
+#  <p> The second one is better because it separates the file reading from the file processing. This makes the code more modular and easier to test. </p>
+# </details>
 #
-# Better to teset the function that modifies mock data in memory than
-# to mock out a service.
+# 2. Which function should you test?
+#
+# <details>
+#  <summary>Answer</summary>
+#  You would have to needlessly mock the file in `work_with_file()` when you could just test the contents.
+#  This may seem obvious here, but always question unit test-level mocks (databases, etc.) and whehter you need them.
+# </details>
--- lectures/003-genai/003-01-genai.py
+++ lectures/003-genai/003-01-genai.py
@@ -9,6 +9,7 @@
 #     - The complete obsolete guide to Generative AI
 #   - Courses
 #     - Lots of courses on Courera
+#     -  Generative AI with Large Language Models by deeplearning.ai
 #     -
 # 1. Learning Project Ideas
 # 1. Gen AI in AWS
